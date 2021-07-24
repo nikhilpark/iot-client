dc7d1d0a4273386780474b65adec1926c283c76d
Nikhil Kumar <1703322.it.cec@cgc.edu.in>
2021-07-24 22:23:59 +0530

    added units

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,41 @@ import "./App.scss";
 function App() {
   const [deviceData, setDeviceData] = useState([]);
   const ENDPOINT = "https://sheru-assign.herokuapp.com/";
-
+  
   useEffect(() => {
     let socket = io(ENDPOINT);
     socket.on("connect", () => {
       console.log("connected");
+      const getData = async () => {
+        const data = await Axios.get("https://sheru-assign.herokuapp.com/getdata");
+        setDeviceData(data.data);
+   
+      };
+      getData();
     });
 
-    const getData = async () => {
-      const data = await Axios.get("/getdata");
-      setDeviceData(data.data);
-    };
-    getData();
-  }, [ENDPOINT]);
+  }, );
+  
 
   return (
     <div id="container">
       <div id="grid-container">
-        <div>DEVICE ID</div>
+        <div>DEVICE ID</div> 
         <div>IMEI</div>
-        <div>Avg. Cell Voltage</div>
-        <div>Pack Voltage</div>
-        <div>Current</div>
+        <div>Avg. Cell Voltage </div>
+        <div>Pack Voltage </div>
+        <div>Current </div>
         <div>Battery Percentage</div>
         {deviceData.map((el, idx) => {
           return (
             <>
-              <div>{el.vid}</div>
+              <div>{el.vid }</div>
               <div>{el.tdata.IMEI}</div>
-              <div>{el.tdata.avgCellVoltage}</div>
-              <div>{el.tdata.packVoltage}</div>
-              <div>{el.tdata.current}</div>
-              <div>{el.tdata.batteryPerc}</div>
-            </>
+              <div>{el.tdata.avgCellVoltage + " mV"}</div>
+              <div>{el.tdata.packVoltage + " mV"}</div>
+              <div>{el.tdata.current + " mAmp"}</div>
+              <div>{el.tdata.batteryPerc + "%"}</div>
+            </> 
           );
         })}
       </div>
